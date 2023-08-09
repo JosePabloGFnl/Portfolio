1804e6edba567f66c37146da859502d801b7ae79
Pablo Gonzalez Flores <jp_glzf@hotmail.com>
2023-08-09 17:36:45 -0600

    Skills class created

---
+++ src/app/mock-skills.ts
@@ -0,0 +1,13 @@
+import { Skill } from './skill';
+
+export const SKILLS: Skill[] = [
+  { id: 1, name: 'Python' },
+  { id: 2, name: 'Julia' },
+  { id: 3, name: 'MySQL' },
+  { id: 4, name: 'Git & GitHub' },
+  { id: 5, name: 'Angular' },
+  { id: 6, name: 'Typescript' },
+  { id: 7, name: 'C & C++' },
+  { id: 8, name: 'C#' },
+  { id: 9, name: 'Combinatorial Optimization' }
+];
