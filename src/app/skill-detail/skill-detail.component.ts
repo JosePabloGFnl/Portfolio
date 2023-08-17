d9b45370f853f1bc7b43295287dae5a634921f65
Pablo González <jp_glzf@hotmail.com>
2023-08-16 19:22:07 -0600

    skill-detail component now visible

--- src/app/skill-detail/skill-detail.component.ts
+++ src/app/skill-detail/skill-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { skill } from '../skill';
 
 @Component({
   selector: 'app-skill-detail',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./skill-detail.component.scss']
 })
 export class SkillDetailComponent {
-
+  @Input() skill?: skill;
 }
