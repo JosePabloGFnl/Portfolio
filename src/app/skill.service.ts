04b1572f0f54159103040f653c376ded3b5ef48a
Pablo González <jp_glzf@hotmail.com>
2023-08-16 20:10:30 -0600

    services and messages added

--- src/app/skill.service.ts
+++ src/app/skill.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { skill } from './skill';
+import { SKILLS } from './mock-skills';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SkillService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
+
+  getSkills(): Observable<skill[]> {
+    const skills = of(SKILLS);
+    this.messageService.add('SkillService: fetched skills');
+    return skills;
+  }
+
 }
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,17 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { skill } from '../skill';
-import { SKILLS } from '../mock-skills';
+import { SkillService } from '../skill.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.scss']
 })
-export class SkillsComponent {
-  skills = SKILLS;
+export class SkillsComponent implements OnInit{
 
   selectedSkill?: skill;
+
+  skills: skill[] = [];
+
+  constructor(private skillService: SkillService, private messageService: MessageService) {}
+
   onSelect(skill: skill): void {
     this.selectedSkill = skill;
+    this.messageService.add('SkillsComponent: Selected skill id=${skill.id}');
   }
+
+  getSkills(): void {
+    this.skillService.getSkills()
+    .subscribe(skills => this.skills = skills);
+  }
+
+  ngOnInit(): void {
+    this.getSkills();
+  }
+
 }
